f48f9f881c0dfa52df0a49ff17f0ba48101598e7
Janek <66107554+creeper82@users.noreply.github.com>
2023-09-25 21:44:54 +0200

    Add even more read, create, remove operations

--- classes/Database.cs
+++ classes/Database.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Flashcards;
 
 public class Database
@@ -10,17 +12,52 @@ public class Database
         Path = db.DbPath;
     }
 
-    public IEnumerable<Deck> getDecks()
+    // READ
+
+    public IEnumerable<Deck> GetDecks()
     {
         var decks = db.Decks
             .OrderBy(Deck => Deck.Name);
         return decks;
     }
 
-    public IEnumerable<Card> getAllCards()
+    public IEnumerable<Card> GetAllCards()
     {
         var cards = db.Cards
             .OrderBy(Card => Card.Id);
         return cards;
+    }
+
+    public IEnumerable<Card> GetDeckCards(Deck Deck)
+    {
+        var cards = Deck.Cards;
+        return cards;
+    }
+
+    // CREATE
+
+    public Deck CreateDeck(string Name)
+    {
+        var Deck = new Deck { Name = Name };
+        db.Add(Deck);
+        db.SaveChanges();
+
+        return Deck;
+    }
+
+    public Card CreateCard(Deck Deck, string Front, string Back)
+    {
+        var Card = new Card { Front = Front, Back = Back, Deck = Deck };
+        Deck.Cards.Add(Card);
+        db.SaveChanges();
+
+        return Card;
+    }
+
+    // REMOVE
+
+    public void ResetAll()
+    {
+        db.Decks.ExecuteDelete();
     }
 }
