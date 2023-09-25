e2559cc4c01873bde54c1612bec048d989893d4e
Janek <66107554+creeper82@users.noreply.github.com>
2023-09-25 21:15:51 +0200

    Add simple database query methods

--- classes/Database.cs
+++ classes/Database.cs
@@ -1,10 +1,26 @@
 namespace Flashcards;
 
-public class Database {
+public class Database
+{
     public FlashcardsContext db = new();
     public string Path = "";
 
-    public Database() {
+    public Database()
+    {
         Path = db.DbPath;
+    }
+
+    public IEnumerable<Deck> getDecks()
+    {
+        var decks = db.Decks
+            .OrderBy(Deck => Deck.Name);
+        return decks;
+    }
+
+    public IEnumerable<Card> getAllCards()
+    {
+        var cards = db.Cards
+            .OrderBy(Card => Card.Id);
+        return cards;
     }
 }
