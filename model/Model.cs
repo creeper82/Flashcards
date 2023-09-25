9f8d6859efcc773e011820fb98bde0e65c4a065d
Janek <66107554+creeper82@users.noreply.github.com>
2023-09-25 17:42:26 +0200

    Update database model

--- model/Model.cs
+++ model/Model.cs
@@ -1,12 +1,29 @@
 namespace Flashcards;
 using Microsoft.EntityFrameworkCore;
 
+
+public class FlashcardsContext : DbContext
+{
+    public DbSet<Deck> Decks { get; set; }
+    public DbSet<Card> Cards { get; set; }
+
+    public string DbPath { get; }
+
+    public FlashcardsContext() {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "flashcards.db");
+    }
+}
+
+
+
 public class Deck
 {
     public int Id { get; set; }
     public string Name { get; set; }
 
-    public List<Card> Cards { get; set; }
+    public List<Card> Cards { get; } = new();
 }
 
 public class Card
@@ -16,6 +33,6 @@ public class Card
     public string Back { get; set; }
 
     public int DeckId { get; set; }
-    public Deck Deck {get; set;} 
+    public Deck Deck { get; set; }
 
 }
