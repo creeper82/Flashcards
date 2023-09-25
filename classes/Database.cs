fed5f05f2e20d0305b898be79f1c7798c63cfff2
Janek <66107554+creeper82@users.noreply.github.com>
2023-09-25 21:09:35 +0200

    Add a simple Database class

--- classes/Database.cs
+++ classes/Database.cs
@@ -1,2 +1,10 @@
 namespace Flashcards;
 
+public class Database {
+    public FlashcardsContext db = new();
+    public string Path = "";
+
+    public Database() {
+        Path = db.DbPath;
+    }
+}
