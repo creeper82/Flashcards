4d7ac38f43c88f8cd0d794696dbdb63641862c8a
Janek <66107554+creeper82@users.noreply.github.com>
2023-09-26 18:36:27 +0200

    Add even more CLI functions

--- classes/CLI.cs
+++ classes/CLI.cs
@@ -1,3 +1,5 @@
+using Flashcards;
+
 namespace CLI;
 
 public static class CLI
@@ -30,9 +32,14 @@ public static class CLI
         );
     }
 
-    private static string Repeat(char Char, int Len)
+    private static string Repeat(char Char, int length)
     {
-        return new String(Char, Len);
+        return new string(Char, length);
+    }
+
+    private static string Spaces(int count)
+    {
+        return Repeat(' ', count);
     }
 
     private static string HorizontalLine(char Char)
@@ -42,52 +49,96 @@ public static class CLI
 
     private static string CenteredText(string text, char SurroundChar = ' ')
     {
-        int SurroundLength = (UiWidth - text.Length) / 2;
+
+        if (text == "") return Repeat(SurroundChar, UiWidth);
+
+        float surroundLength = (UiWidth - text.Length) / 2f;
+
+        Console.WriteLine($"\n{text} -- ui width is {UiWidth}, surround length is {surroundLength}, text width is {text.Length}\n");
 
         return (
-            Repeat(SurroundChar, SurroundLength) +
+            Repeat(SurroundChar, (int)Math.Ceiling(surroundLength)) +
             text +
-            Repeat(SurroundChar, SurroundLength)
+            Repeat(SurroundChar, (int)Math.Floor(surroundLength))
         );
     }
 
     private static string MultilineCenteredText(string Text)
     {
-        var Lines = Text.Split("\n");
-        string LastLine = Lines.Last();
-        string Result = "";
+        var lines = Text.Split("\n");
+        string result = "";
 
-        foreach (string Line in Lines)
+        for (int i = 0; i < lines.Length; i++)
         {
-            Result += CenteredText(Line);
-            if (Line != LastLine) Result += "\n";
+            result += CenteredText(lines[i]);
+            if (i < lines.Length - 1) result += "\n";
         }
 
-        return Result;
+        return result;
 
 
     }
 
-    private static string UiFrame(string Inner, string Title = "")
+    private static string DeckList(IEnumerable<Deck> decks)
+    {
+        var list = decks.Select(deck => deck.Name);
+        return List(list);
+    }
+
+    // Create a list with top+bottom dashed border and elements inside
+    private static string List(IEnumerable<String> strings)
+    {
+        int width = 0;
+
+        // Determine element with largest width
+        foreach (string elem in strings)
+        {
+            if (elem.Length > width) width = elem.Length;
+        }
+
+        if (width == 0) return "";
+        else
+        {
+            // Top border
+            string list = "\n" + Repeat('-', width);
+
+            // Elements
+            foreach (string elem in strings)
+            {
+                list += "\n" + elem;
+                if (elem.Length < width) list += Spaces(width - elem.Length);
+            }
+
+            // Bottom border
+            list += "\n" + Repeat('-', width);
+
+            return MultilineCenteredText(list);
+        }
+    }
+
+    private static string UiFrame(string inner, string title = "")
     {
         return (
             CenteredText(
                 // If title is not empty, then add a margin to it
-                Title != "" ? Title.Margin() : "",
+                title != "" ? title.Margin() : "",
                 '-'
             ) + "\n\n" +
-            Inner + "\n\n" +
+            inner + "\n\n" +
             HorizontalLine('-')
         );
 
     }
 
-    public static void Menu()
+    public static void Menu(IEnumerable<Deck> decks)
     {
         Console.Clear();
         Console.WriteLine(
             UiFrame(
-                MultilineCenteredText("Hello. Welcome to Flashcards\nHave a good day"),
+                (
+                    MultilineCenteredText("Welcome to Flashcards!\nHere are your decks:\n") +
+                    DeckList(decks)
+                ),
                 "Flashcards")
         );
     }
