fd300ca404063d684f1351302c6fbc3e97280002
Janek <66107554+creeper82@users.noreply.github.com>
2023-09-26 10:03:44 +0200

    Add basic CLI functions

--- classes/CLI.cs
+++ classes/CLI.cs
@@ -1,1 +1,27 @@
 namespace CLI;
+
+public static class CLI {
+    public static int UiWidth = 64;
+
+    private static string Repeat(char Char, int Len) {
+        return new String(Char, Len);
+    }
+
+    private static string CenteredText(string text, char SurroundChar = ' ') {
+        int SurroundLength = UiWidth - (text.Length * 2);
+        return (
+            Repeat(SurroundChar, SurroundLength) +
+            text +
+            Repeat(SurroundChar, SurroundLength)
+            );
+    }
+
+    public static void UiFrame(string Inner, string Title = "") {
+        
+
+    }
+
+    public static void Menu() {
+
+    }
+}
