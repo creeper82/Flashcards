6dd5cbe45013708cca1baa1a217d310838c6b917
Janek <66107554+creeper82@users.noreply.github.com>
2023-09-26 17:44:25 +0200

    Remove database test in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,21 +1,17 @@
 ﻿using Flashcards;
+using FlashcardsApp;
 using Microsoft.Data.Sqlite;
 
-var db = new Database();
+var db = new FlashcardsDatabase();
 
 Console.WriteLine($"Database path is {db.Path}");
 
 try
 {
-    db.ResetAll();
-
-    var Deck = db.CreateDeck("Sample deck");
-    db.CreateCard(Deck, "Card_front", "Card_back");
-
-    var Card = db.GetDeckCards(Deck).First();
-    Console.WriteLine($"Card front is {Card.Front} and deck name is {Card.Deck.Name}");
+    App.Start(db);
 }
 catch (SqliteException)
 {
     Console.WriteLine("ERROR. The database is corrupt");
+    throw;
 }
