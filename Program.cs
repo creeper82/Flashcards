c84484dc3ae5a4096489ba8cc8c9116d4692880d
Janek <66107554+creeper82@users.noreply.github.com>
2023-09-26 09:48:38 +0200

    Add simple exception

--- Program.cs
+++ Program.cs
@@ -1,13 +1,21 @@
 ﻿using Flashcards;
+using Microsoft.Data.Sqlite;
 
 var db = new Database();
 
 Console.WriteLine($"Database path is {db.Path}");
 
-db.ResetAll();
+try
+{
+    db.ResetAll();
 
-var Deck = db.CreateDeck("Sample deck");
-db.CreateCard(Deck, "Card_front", "Card_back");
+    var Deck = db.CreateDeck("Sample deck");
+    db.CreateCard(Deck, "Card_front", "Card_back");
 
-var Card = db.GetDeckCards(Deck).First();
-Console.WriteLine($"Card front is {Card.Front} and deck name is {Card.Deck.Name}");
+    var Card = db.GetDeckCards(Deck).First();
+    Console.WriteLine($"Card front is {Card.Front} and deck name is {Card.Deck.Name}");
+}
+catch (SqliteException)
+{
+    Console.WriteLine("ERROR. The database is corrupt");
+}
