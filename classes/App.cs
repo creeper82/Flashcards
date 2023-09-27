f16eead4193656bd3e60502b534b997b180f98ad
Janek <66107554+creeper82@users.noreply.github.com>
2023-09-27 17:52:07 +0200

    Display decks list controllable with arrow keys

--- classes/App.cs
+++ classes/App.cs
@@ -3,14 +3,28 @@ namespace FlashcardsApp;
 using Flashcards;
 using CLI;
 
-public static class App {
-    public static void Start(FlashcardsDatabase database) {
+public static class App
+{
+    public static void Start(FlashcardsDatabase database)
+    {
         database.ResetAll();
         database.CreateDeck("Aaaa");
         database.CreateDeck("Sample dec");
         database.CreateDeck("Another deck");
 
-        int selectedDeckIndex = 0;
-        CLI.Menu(database.GetDecks(), selectedDeckIndex);
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        CLI.ChoiceList<Deck> deckChoiceList = new(database.GetDecks());
+        while (true)
+        {
+            CLI.Menu(deckChoiceList.choices, deckChoiceList.selectedIndex);
+            
+            try
+            {
+                deckChoiceList.waitForArrowKey();
+            }
+            catch (InvalidOperationException) { break; }
+        }
+
     }
 }
