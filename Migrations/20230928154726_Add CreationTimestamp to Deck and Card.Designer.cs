f7ca768e363a912a79174cbd718754c99c9e48e6
Janek <66107554+creeper82@users.noreply.github.com>
2023-09-28 17:48:05 +0200

    Add migration files (CreationTimestamp)

--- Migrations/20230928154726_Add CreationTimestamp to Deck and Card.Designer.cs
+++ Migrations/20230928154726_Add CreationTimestamp to Deck and Card.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Flashcards;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Flashcards.Migrations
 {
     [DbContext(typeof(FlashcardsContext))]
-    partial class FlashcardsContextModelSnapshot : ModelSnapshot
+    [Migration("20230928154726_Add CreationTimestamp to Deck and Card")]
+    partial class AddCreationTimestamptoDeckandCard
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.11");
