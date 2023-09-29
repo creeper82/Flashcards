21233edf3442b9cf3eb6d8c5f7ac17a60f0fa524
tam <cos@gmail.com>
2023-09-29 12:14:37 +0200

    Add Dialogs class

---
+++ classes/CLI/Dialogs.cs
@@ -0,0 +1,16 @@
+namespace CLI;
+
+public static class Dialogs {
+    private static class DialogScreens {
+        private static void ConfirmScreen {
+
+        }
+    }
+    public static bool Confirm(
+        string title,
+        string message = "",
+        string okButton = "OK",
+        string cancelButton = "Cancel"
+    ) {
+    }
+}
