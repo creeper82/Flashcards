7e4ad61f626936e06d2cd07b4e48be20d157e02c
tam <cos@gmail.com>
2023-09-29 12:14:26 +0200

    Add deck edit options

--- classes/CLI/Options.cs
+++ classes/CLI/Options.cs
@@ -4,6 +4,8 @@ public static class Options
 {
     public static List<CLI.Option> MenuOptions { get; } = new() {
         new("up/down", "move selection"),
-        new("enter", "open deck")
+        new("enter", "open deck"),
+        new("del", "delete deck"),
+        new("n", "rename deck")
     };
 }
