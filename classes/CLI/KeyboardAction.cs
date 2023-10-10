09f8f9af3cc1b3c29827d116687f800c4fb0693c
Janek <66107554+creeper82@users.noreply.github.com>
2023-10-10 18:01:47 +0200

    Add CardEditor keyboard actions

--- classes/CLI/KeyboardAction.cs
+++ classes/CLI/KeyboardAction.cs
@@ -1,25 +1,27 @@
 namespace CLI;
 
+// KeyboardAction represents a key you have to press and a name of action it'll trigger
 public class KeyboardAction
 {
-    public string key = "";
-    public string optionText = "";
+    public string Key = "";
+    public string OptionText = "";
 
     public KeyboardAction(string key, string optionText)
     {
-        this.key = key;
-        this.optionText = optionText;
+        Key = key;
+        OptionText = optionText;
     }
 
     public override string ToString()
     {
         // Empty option string is used as a line separator
-        return (key == "" && optionText == "") ? "" : $"[ {key} ] - {optionText}";
+        return (Key == "" && OptionText == "") ? "" : $"[ {Key} ] - {OptionText}";
     }
 
     public static KeyboardAction LineSeparator => new("", "");
 }
-// List of commonly used keyboard actions in screens
+
+// List of commonly used keyboard actions among app screens
 public static class KeyboardActions
 {
     public static List<KeyboardAction> DeckListScreen { get; } = new() {
@@ -45,13 +47,27 @@ public static class KeyboardActions
         KeyboardAction.LineSeparator,
         new("esc", "go back")
     };
-    
+
     public static List<KeyboardAction> DeckScreenEmpty { get; } = new() {
         new("del", "delete deck"),
         new("r", "rename deck"),
         new("c", "cards in deck"),
         new("i", "deck details"),
         KeyboardAction.LineSeparator,
+        new("esc", "go back")
+    };
+
+    public static List<KeyboardAction> CardEditorScreen { get; } = new() {
+        new("left/right", "move selection"),
+        new("enter", "edit card"),
+        new("del", "delete card"),
+        KeyboardAction.LineSeparator,
+        new("n", "create new card"),
+        new("esc", "go back")
+    };
+
+    public static List<KeyboardAction> CardEditorScreenEmpty { get; } = new() {
+        new("n", "create new card"),
         new("esc", "go back")
     };
 }
