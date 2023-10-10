ff190cc77a8bcd3ad87192494347105ce1a663a9
Janek <66107554+creeper82@users.noreply.github.com>
2023-10-10 17:50:01 +0200

    Move DeckCardList to separate file

--- classes/app/screens/Deck.cs
+++ classes/app/screens/Deck.cs
@@ -16,24 +16,4 @@ public static partial class App
         }
 
     }
-
-    public static void DeckCardList(FlashcardsDatabase database, Deck deck)
-    {
-        ChoiceList<Card> cardChoiceList = new(deck.Cards);
-
-        bool running = true;
-
-        while (running)
-        {
-            cardChoiceList.CheckOutOfBoundsPointer();
-            Screens.CardEditor(
-                card: cardChoiceList.SelectedItem,
-                currentCardNumber: cardChoiceList.selectedIndex + 1,
-                maxCardNumber: cardChoiceList.MaxIndex + 1,
-                deckName: deck.Name
-            );
-
-            ConsoleKey consoleKey = Console.ReadKey().Key;
-        }
-    }
 }
