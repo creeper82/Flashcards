cbc66eedd0dd1786ec8af17d6bd9d4bbda60d37d
Janek <66107554+creeper82@users.noreply.github.com>
2023-10-11 21:32:24 +0200

    Implement basic pagination

--- classes/app/screens/Start.cs
+++ classes/app/screens/Start.cs
@@ -9,23 +9,35 @@ public static partial class App
     {
         database.ResetAll();
         // sample decks
-        database.CreateDeck("Angol");
-        database.CreateDeck("Sample dec");
-        database.CreateDeck("Another deck");
+        database.CreateDeck("Deck #01");
+        database.CreateDeck("Deck #02");
+        database.CreateDeck("Deck #03");
+        database.CreateDeck("Deck #04");
+        database.CreateDeck("Deck #05");
+        database.CreateDeck("Deck #06");
+        database.CreateDeck("Deck #07");
+        database.CreateDeck("Deck #08");
+        database.CreateDeck("Deck #09");
+        database.CreateDeck("Deck #10");
+        database.CreateDeck("Deck #11");
+        database.CreateDeck("Deck #12");
         // sample cards
         database.CreateCard(database.GetDecks().First(), "feeble", "słaby");
         database.CreateCard(database.GetDecks().First(), "shrubs", "kitel");
 
         Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-        ChoiceList<Deck> deckChoiceList = new(database.GetDecks());
+        ChoiceList<Deck> deckChoiceList = new(database.GetDecks())
+        {
+            PaginationCount = 5
+        };
 
         bool running = true;
 
         while (running)
         {
             deckChoiceList.CheckOutOfBoundsPointer();
-            Screens.Menu(deckChoiceList.choices, deckChoiceList.selectedIndex);
+            Screens.Menu(deckChoiceList.PaginatedChoices, deckChoiceList.selectedIndex, deckChoiceList.PaginationStartIndex);
             
             running = Interactions.HandleMenu(database, deckChoiceList);
         }
