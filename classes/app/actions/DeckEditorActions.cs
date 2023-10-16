5f717961d92d8bae3a2c56f083d5e97a54e0e3d9
Janek <66107554+creeper82@users.noreply.github.com>
2023-10-16 20:34:44 +0200

    Update DeckEditorActions.cs

--- classes/app/actions/DeckEditorActions.cs
+++ classes/app/actions/DeckEditorActions.cs
@@ -4,26 +4,31 @@ using Flashcards;
 
 public static partial class Interactions
 {
-    private static void RemoveCardAction(FlashcardsDatabase database, Card card) {
+    private static void RemoveCardAction(FlashcardsDatabase database, Card card)
+    {
 
-        if(CLI.Dialogs.Confirm(
+        if (CLI.Dialogs.Confirm(
             title: "Remove card",
             message: "Do you want to remove this card?",
             okButton: "remove",
             cancelButton: "cancel"
-        )) {
+        ))
+        {
             database.RemoveCard(card);
         }
     }
 
-    private static void EditCardAction(FlashcardsDatabase database, Card card) {
+    private static void EditCardAction(FlashcardsDatabase database, Card card)
+    {
         var editedCard = App.CardEditor(database, card);
         database.UpdateCard(card, editedCard.Front, editedCard.Back);
     }
 
-    private static Card? CreateCardAction(FlashcardsDatabase database, Deck deck) {
+    private static Card? CreateCardAction(FlashcardsDatabase database, Deck deck)
+    {
         var newCard = App.CardEditor(database, Card.EmptyCard(deck), "New card");
-        if (newCard.Front != "" && newCard.Back != "") {
+        if (newCard.Front != "" && newCard.Back != "")
+        {
             database.AppendCard(newCard);
             return newCard;
         }
