520119a7268b9eea16f7aef4f51b8ee440847527
Janek <66107554+creeper82@users.noreply.github.com>
2023-10-24 16:50:33 +0200

    Split the Filtering class into two files

--- classes/database/filtering/ApplyFilter.cs
+++ classes/database/filtering/ApplyFilter.cs
@@ -1,45 +1,12 @@
 namespace Flashcards;
 
-public static class Filtering
+public static partial class Filtering
 {
-    // Determines which part of the card should be matched to the keyword
-    public enum KeywordMatchMode
-    {
-        Any, CardFront, CardBack
-    }
-
-    public static string FriendlyName(this KeywordMatchMode matchMode) {
-        return matchMode switch
-        {
-            KeywordMatchMode.Any => "Search the whole card",
-            KeywordMatchMode.CardFront => "Search the card front",
-            KeywordMatchMode.CardBack => "Search the card back",
-            _ => "Search",
-        };
-    }
-
-    public class CardFilter
-    {
-
-        public KeywordMatchMode MatchMode = KeywordMatchMode.Any;
-        public string? Keyword = null;
-        public int? RecentDays = null;
-
-        public CardFilter(string? keyword = null, KeywordMatchMode matchMode = 0, int? recentDays = null)
-        {
-            MatchMode = matchMode;
-            Keyword = keyword;
-            RecentDays = recentDays;
-        }
-
-    }
-
     public static IEnumerable<Card> ApplyFilter(this IEnumerable<Card> cards, CardFilter cardFilter)
     {
-
         IEnumerable<Card> filteredCards = cards;
 
-        // filter cards by keyword
+        // filter the cards by keyword
         if (cardFilter.Keyword is not null)
         {
             filteredCards = cards.Where(card =>
@@ -58,7 +25,7 @@ public static class Filtering
         });
         }
 
-        // filter cards by date
+        // filter the cards by date
         if (cardFilter.RecentDays is not null)
         {
             filteredCards = cards.Where(card => (DateTime.UtcNow - card.CreationTimestamp).Days <= cardFilter.RecentDays);
---
+++ classes/database/filtering/CardFilter.cs
@@ -0,0 +1,39 @@
+namespace Flashcards;
+
+public static partial class Filtering
+{
+    // Determines which part of the card should be matched to the keyword
+    public enum KeywordMatchMode
+    {
+        Any, CardFront, CardBack
+    }
+
+    // Returns a friendly name for a KeywordMatchMode
+    public static string FriendlyName(this KeywordMatchMode matchMode) {
+        return matchMode switch
+        {
+            KeywordMatchMode.Any => "Search the whole card",
+            KeywordMatchMode.CardFront => "Search the card front",
+            KeywordMatchMode.CardBack => "Search the card back",
+            _ => "Search",
+        };
+    }
+
+    public class CardFilter
+    {
+
+        public KeywordMatchMode MatchMode = KeywordMatchMode.Any;
+        public string? Keyword = null;
+        public int? RecentDays = null;
+
+        public CardFilter(string? keyword = null, KeywordMatchMode matchMode = 0, int? recentDays = null)
+        {
+            MatchMode = matchMode;
+            Keyword = keyword;
+            RecentDays = recentDays;
+        }
+
+    }
+
+    
+}
